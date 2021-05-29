843d29111cb6894d626d291e6a182bf062ec26da
Youn Seung Hwan <cxzyy62@gmail.com>
2021-05-29 13:29:26 +0900

    adress ilike

--- src/stores/seeStores/seeStores.resolvers.ts
+++ src/stores/seeStores/seeStores.resolvers.ts
@@ -2,13 +2,23 @@ import client from "../../client"
 
 export default {
     Query :{
-        seeStores: (_, { address }) =>    
-            client.store.findMany({
-                where:{
-                    address
-                },
+        seeStores: async(_, { address }) =>  {
+        const dsa =address.split(" ");
+        const address_search = dsa[0]+" "+dsa[1]+" "+dsa[2];
+        const result = await client.$queryRaw(
+            'SELECT * FROM "Store" WHERE address ilike $1 ORDER BY state DESC', // Using %$2 here would not work
+            `%${address_search}%`, 
+        );
+        return result;
+        }
+
+
+        }  
+            // client.store.findMany({
+            //     where:{
+            //         address
+            //     },
                 
-            }),
-              
-}
+            // }),
+            //}
 }
