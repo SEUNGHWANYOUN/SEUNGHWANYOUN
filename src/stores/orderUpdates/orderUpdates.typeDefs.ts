92256b5696f46176f48dfac6f873b37f339404ab
Youn Seung Hwan <cxzyy62@gmail.com>
2021-06-30 23:47:35 +0900

    subscript orderlist

--- src/stores/orderUpdates/orderUpdates.typeDefs.ts
+++ src/stores/orderUpdates/orderUpdates.typeDefs.ts
@@ -2,7 +2,7 @@ import { gql } from "apollo-server";
 
 export default gql`
  type Subscription {
-   
+  #  배열로했을때 값 못가져옴 error 이상하게 나옴 찾을수 없다고 orderUpdates
    orderUpdates(storeId: Int!): Order
  }
 `;
